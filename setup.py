35eaad13d4fa33ddd9ef6f71564d1e82374a2925
Mike Huls <m.huls@datanext.nl>
2021-12-07 15:53:26 +0100

    corrected setup.py for version 0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='cythonbuilder',  # should match the package folder
     packages=['cythonbuilder'],  # should match the package folder
-    version='0.0.1',  # important for updates
+    version='0.0.2',  # important for updates
     license='MIT',  # should match your chosen license
     description='CythonBuilder; automated compiling and packaging of Cython code',
     long_description=long_description,  # loads your README.md
@@ -31,5 +31,5 @@ setuptools.setup(
         'Programming Language :: Python :: 3.9',
     ],
 
-    download_url="https://github.com/mike-huls/cythonbuilder/archive/refs/tags/v0.0.1.tar.gz",
+    download_url="https://github.com/mike-huls/cythonbuilder/archive/refs/tags/v0.0.2.tar.gz",
 )
