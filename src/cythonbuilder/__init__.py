baa547d5c1feffe5ef92ae09ed567e18d19faa10
Mike Huls <m.huls@datanext.nl>
2023-04-24 21:33:34 +0200

    built 0.0.16

--- src/cythonbuilder/__init__.py
+++ src/cythonbuilder/__init__.py
@@ -1,7 +1,7 @@
 # import code from your package here to make them available outside the module like:
 
-__version__ = "0.1.15"
-__last_build_datetime__ ="2023-03-25 13:11:45"
-__last_publish_version__ ="0.1.15"
-__last_publish_datetime__ ="2023-03-25 13:12:43"
+__version__ = "0.0.16"
+__last_build_datetime__ ="2023-04-24 21:32:24"
+__last_publish_version__ ="0.0.16"
+__last_publish_datetime__ ="2023-04-24 21:33:16"
 
