34b0ac9105c840da28886880752d62f7d6f9f679
Mike Huls <m.huls@datanext.nl>
2023-04-29 13:00:17 +0200

    installed poetry

--- src/cythonbuilder/__init__.py
+++ src/cythonbuilder/__init__.py
@@ -1,7 +1,7 @@
 # import code from your package here to make them available outside the module like:
 
-__version__ = "0.0.17"
-__last_build_datetime__ ="2023-04-25 08:40:56"
-__last_publish_version__ ="0.0.17"
-__last_publish_datetime__ ="2023-04-25 08:42:22"
+__version__ = "0.0.19"
+__last_build_datetime__ ="2023-04-28 17:02:38"
+__last_publish_version__ ="0.0.18"
+__last_publish_datetime__ ="2023-04-28 16:48:26"
 
